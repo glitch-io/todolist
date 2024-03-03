7fdc6487fb52fea944c085286cea51b3a039ce40
TechBitsArmy <200305105118@paruluniversity.ac.in>
2024-03-03 16:27:57 +0530

    .

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,10 +2,10 @@ function Task({ todo, toggleComplete, handleDelete }) {
   return (
     <li
       key={todo.id}
-      className="g my-3 flex items-center justify-between gap-x-6 overflow-auto rounded-lg bg-slate-200 p-4 py-3
+      className="g my-3 flex items-center justify-between gap-x-6 overflow-auto rounded-lg border border-black p-4 py-3
       shadow-lg dark:bg-slate-800"
     >
-      <div className="flex gap-2 text-cyan-50">
+      <div className="flex gap-2 text-black dark:text-cyan-50">
         <input
           type="checkbox"
           checked={todo.completed}
@@ -14,7 +14,7 @@ function Task({ todo, toggleComplete, handleDelete }) {
         <p className="text-base font-normal">{todo.text}</p>
       </div>
       <button
-        className=" inline-block rounded-md border border-[1] border-red-800 bg-slate-800 px-2 py-0.5 text-red-800 hover:bg-red-600 hover:text-slate-100"
+        className=" inline-block rounded-md border border-[1] border-red-800 px-2 py-0.5 text-red-800 hover:bg-red-600 hover:text-slate-100 dark:bg-slate-800"
         onClick={() => handleDelete(todo.id)}
       >
         Delete
