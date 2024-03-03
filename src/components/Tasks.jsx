aedca131c6d403a1afdeef6f8d8d7261d194c21f
TechBitsArmy <200305105118@paruluniversity.ac.in>
2024-03-03 16:06:37 +0530

    Center Align

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,5 @@
 import Task from "./Task";
 import { verse } from "../assets/verse.json";
-
 let randomId = Math.floor(Math.random() * 701) + 1;
 let shlok = verse[randomId].text;
 function Tasks({ todos, toggleComplete, handleDelete }) {
@@ -9,7 +8,11 @@ function Tasks({ todos, toggleComplete, handleDelete }) {
       <h2 className="my-2 text-2xl font-semibold">Your Task</h2>
       <div className="not-prose relative overflow-hidden rounded-xl bg-slate-800/25 ">
         <ul className="bg-grid-slate-700/25 inset-0 px-3">
-          {todos.length === 0 && <p className="p-4">{shlok}</p>}
+          {todos.length === 0 && (
+            <p className="flex items-center justify-center p-4 text-center">
+              {shlok}
+            </p>
+          )}
           {todos.map((todo) => (
             <Task
               key={todo.id}
